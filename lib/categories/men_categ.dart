5b761868cdbd5a5b277cce375774a485ec89593f
FarhanABD <farhanabdilah204@gmail.com>
2023-01-19 05:08:30 +0700

    Section:1 Adding Slidebar

--- lib/categories/men_categ.dart
+++ lib/categories/men_categ.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/minor_screen/subcateg_product.dart';
 import 'package:multi_store_app/utilities/categ_list.dart';
 
 class MenCategory extends StatelessWidget {
@@ -6,40 +7,115 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Text(
-            "Men",
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),
-          ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.width * 0.68,
-          child: GridView.count(
-            mainAxisSpacing: 70,
-            crossAxisSpacing: 15,
-            crossAxisCount: 3,
-            children: List.generate(men.length, (index) {
-              return Column(
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    height: 70,
-                    width: 70,
-                    child: Image(
-                      image: AssetImage('images/men/men$index.jpg'),
+                  const Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      "Men",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5),
                     ),
                   ),
-                  Text(men[index])
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.68,
+                    child: GridView.count(
+                      mainAxisSpacing: 70,
+                      crossAxisSpacing: 15,
+                      crossAxisCount: 3,
+                      children: List.generate(men.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SubCategProducts(
+                                          maincategName: 'men',
+                                          subcategName: men[index],
+                                        )));
+                          },
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 70,
+                                width: 70,
+                                child: Image(
+                                  image: AssetImage('images/men/men$index.jpg'),
+                                ),
+                              ),
+                              Text(men[index])
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+                  )
                 ],
-              );
-            }),
+              ),
+            ),
           ),
-        )
-      ],
+          Positioned(
+              bottom: 0,
+              right: 0,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.brown.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Text(
+                            " << ",
+                            style: TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          ),
+                          Text(
+                            "men".toUpperCase(),
+                            style: const TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          ),
+                          const Text(
+                            " >> ",
+                            style: TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
